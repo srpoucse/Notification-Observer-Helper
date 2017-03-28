cdd9f65a8eb05d161593968ca0ca8dd9d8ebce72
Ratna Saka <srpoucse89@gmail.com>
2017-03-28 12:22:13 -0400

    Initial Commit

---
+++ NotificationObserverHelper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NotificationObserverHelper
+//
+//  Created by Ratna Saka on 3/28/17.
+//  Copyright © 2017 srp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
